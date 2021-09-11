b6fb93bf071f7a8e2aaf5c16d8bd4d860157a912
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-09-11 13:26:35 +0530

    Add files via upload

---
+++ BinarySearchTree/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int info;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int value)
+{
+    struct node *n;
+
+    n = (struct node *)malloc(sizeof(struct node));
+
+    n->info = value;
+    n->left = NULL;
+    n->right = NULL;
+
+    return n;
+};
+
+struct node *insert(struct node *root, int value)
+{
+    if(root == NULL)
+    {
+        return createNode(value);
+    }
+
+    if(value < root->info)
+    {
+        root->left = insert(root->left, value);
+    }
+    else if(value > root->info)
+    {
+        root->right = insert(root->right, value);
+    }
+
+};
+
+void inOrder(struct node *root)
+{
+    if(root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d  ", root->info);
+        inOrder(root->right);
+    }
+}
+
+void preOrder(struct node *root)
+{
+    if(root != NULL)
+    {
+        printf("%d  ", root->info);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct node *root)
+{
+    if(root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d  ", root->info);
+    }
+}
+
+
+int main()
+{
+    struct node *root = NULL;
+
+    root = insert(root, 8);
+    insert(root, 10);
+    insert(root, 7);
+    insert(root, 5);
+    insert(root, 17);
+    insert(root, 25);
+
+    printf("\nInorder Traversal\n");
+    inOrder(root);
+
+
+    printf("\nPreorder Traversal\n");
+    preOrder(root);
+
+
+    printf("\nPostorder Traversal\n");
+    postOrder(root);
+
+    return 0;
+}
